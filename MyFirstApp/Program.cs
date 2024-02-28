ca57cfc8f672980a202f857f13d13704e94167f2
Markus Rei <markus.reinumae@tktk.ee>
2024-02-28 14:44:01 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is a comment
+//rakendus küsib kasutajal sisestada oma nimi
+//rakendus tervitab kasutajat nimepidi
+
+
+Console.WriteLine("Enter your name"); //Output
+
+//string = sõna
+string userName = Console.ReadLine(); //Input
+
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
